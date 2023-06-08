c83dcdc6ef9f1fd548c8606194ecf6de6b4d99ed
DevilKrishna009 <krishnakantsahoo000@gmail.com>
2023-06-08 10:57:39 +0530

    Refreshed

--- django_intro/project/User_Assignment/models.py
+++ django_intro/project/User_Assignment/models.py
@@ -8,6 +8,8 @@ class UserModel(models.Model):
     name = models.CharField(max_length=30)
     contact = models.IntegerField()
 
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+
     def __str__(self):
         return self.name
 
@@ -20,12 +22,16 @@ class UserProfileModel(models.Model):
     gender = models.CharField(max_length=1)
     university = models.CharField(max_length=30)
 
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+
 
 class UserPostModel(models.Model):
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=100)
     user_des = models.ForeignKey(UserModel, on_delete=models.CASCADE, null=True, blank=True,
                                  related_name="user_post_model")
+
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
 
     def __str__(self):
         return self.title
